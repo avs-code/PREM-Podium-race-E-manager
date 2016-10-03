655e14a4a89521c3207dab9e184f59ebf9c0387b
arv187 <sparkticus20@hotmail.com>
2016-10-04 00:56:09 +0200

    updated utf8

--- mysql_structure.sql
+++ mysql_structure.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: sql101.0lx.net
--- Tiempo de generación: 03-10-2016 a las 07:31:52
+-- Tiempo de generación: 03-10-2016 a las 18:53:16
 -- Versión del servidor: 5.6.21-69.0
 -- Versión de PHP: 5.3.3
 
@@ -28,8 +28,8 @@ SET time_zone = "+00:00";
 
 CREATE TABLE IF NOT EXISTS `division` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(30) NOT NULL DEFAULT '',
-  `type` varchar(30) NOT NULL DEFAULT '',
+  `name` varchar(30) CHARACTER SET utf8 NOT NULL DEFAULT '',
+  `type` varchar(30) CHARACTER SET utf8 NOT NULL DEFAULT '',
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=6 ;
 
@@ -41,11 +41,11 @@ CREATE TABLE IF NOT EXISTS `division` (
 
 CREATE TABLE IF NOT EXISTS `driver` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(30) NOT NULL DEFAULT '',
+  `name` varchar(30) CHARACTER SET utf8 NOT NULL DEFAULT '',
   `1st` int(30) NOT NULL,
   `2nd` int(30) NOT NULL,
   `3rd` int(30) NOT NULL,
-  `driver_photo` varchar(200) NOT NULL,
+  `driver_photo` varchar(200) CHARACTER SET utf8 NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=60 ;
 
@@ -57,7 +57,7 @@ CREATE TABLE IF NOT EXISTS `driver` (
 
 CREATE TABLE IF NOT EXISTS `point_ruleset` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(30) NOT NULL DEFAULT '',
+  `name` varchar(30) CHARACTER SET utf8 NOT NULL DEFAULT '',
   `rp1` int(11) NOT NULL DEFAULT '0',
   `rp2` int(11) NOT NULL DEFAULT '0',
   `rp3` int(11) NOT NULL DEFAULT '0',
@@ -90,8 +90,8 @@ CREATE TABLE IF NOT EXISTS `point_ruleset` (
 
 CREATE TABLE IF NOT EXISTS `race` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(30) NOT NULL DEFAULT '',
-  `track` varchar(30) NOT NULL DEFAULT '',
+  `name` varchar(30) CHARACTER SET utf8 NOT NULL DEFAULT '',
+  `track` varchar(30) CHARACTER SET utf8 NOT NULL DEFAULT '',
   `laps` int(11) NOT NULL DEFAULT '0',
   `season` int(11) NOT NULL DEFAULT '0',
   `division` int(11) NOT NULL DEFAULT '0',
@@ -101,7 +101,7 @@ CREATE TABLE IF NOT EXISTS `race` (
   `maxplayers` int(11) NOT NULL DEFAULT '0',
   `result_official` tinyint(1) NOT NULL DEFAULT '0',
   `progress` int(11) NOT NULL DEFAULT '0',
-  `imagelink` varchar(200) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `imagelink` varchar(200) CHARACTER SET utf8 NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=14 ;
 
@@ -131,7 +131,7 @@ CREATE TABLE IF NOT EXISTS `race_driver` (
 
 CREATE TABLE IF NOT EXISTS `season` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(20) NOT NULL DEFAULT '',
+  `name` varchar(20) CHARACTER SET utf8 NOT NULL DEFAULT '',
   `division` int(11) NOT NULL DEFAULT '0',
   `ruleset` int(11) NOT NULL DEFAULT '0',
   `ruleset_qualifying` int(11) NOT NULL DEFAULT '0',
@@ -159,9 +159,9 @@ CREATE TABLE IF NOT EXISTS `season_team` (
 
 CREATE TABLE IF NOT EXISTS `sim_results` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `race_name` varchar(30) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `race_name` varchar(30) CHARACTER SET utf8 NOT NULL,
   `season` int(11) NOT NULL DEFAULT '0',
-  `simresults_url` varchar(200) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `simresults_url` varchar(200) CHARACTER SET utf8 NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=8 ;
 
@@ -173,8 +173,8 @@ CREATE TABLE IF NOT EXISTS `sim_results` (
 
 CREATE TABLE IF NOT EXISTS `team` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(30) NOT NULL DEFAULT '',
-  `logo` varchar(200) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `name` varchar(30) CHARACTER SET utf8 NOT NULL DEFAULT '',
+  `logo` varchar(200) CHARACTER SET utf8 NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=28 ;
 
@@ -199,8 +199,8 @@ CREATE TABLE IF NOT EXISTS `team_driver` (
 
 CREATE TABLE IF NOT EXISTS `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(20) NOT NULL DEFAULT '',
-  `passwd` varchar(40) NOT NULL DEFAULT '',
+  `name` varchar(20) CHARACTER SET utf8 NOT NULL DEFAULT '',
+  `passwd` varchar(40) CHARACTER SET utf8 NOT NULL DEFAULT '',
   `active` tinyint(1) NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=6 ;
@@ -213,8 +213,8 @@ CREATE TABLE IF NOT EXISTS `user` (
 
 CREATE TABLE IF NOT EXISTS `video` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `video_name` varchar(30) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
-  `video_url` varchar(200) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `video_name` varchar(30) CHARACTER SET utf8 NOT NULL,
+  `video_url` varchar(200) CHARACTER SET utf8 NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
 
