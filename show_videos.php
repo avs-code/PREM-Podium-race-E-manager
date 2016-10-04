640523aee76718f2f919315f29ab9cab316395fe
inguni <inguni@gmail.com>
2016-10-04 14:58:47 +0200

    Fixed showing videos
    Modified all about showing videos, now it should run.

--- show_videos.php
+++ show_videos.php
@@ -1,19 +1,18 @@
 <? if (!defined("CONFIG"))
-    exit(); ?>
-<?
-$video = "SELECT `video`.`video_name` FROM video ORDER BY `video`.`id` DESC LIMIT 0 , 30";
+    exit();
+
+$video = "SELECT `id`, `video_name`, `video_url` FROM video ORDER BY `id` DESC LIMIT 0 , 30";
 $result = mysql_query($video);
 if (!$result) {
     show_error("MySQL Error: " . mysql_error() . "\n");
     return;
 }
-?>
-<?
-while ($sitem = mysql_fetch_array($result)) { ?>
-<?php
-$url = 'video_url';
-?>
 
-<iframe id="ytplayer" type="text/html" width="420" height="345"
-    src="<?php echo $url; ?>" frameborder="0" allowfullscreen></iframe>;
+while ($sitem = mysql_fetch_array($result)) {
+	$url = $sitem['video_url'];
+	?>
+	<iframe id="ytplayer" type="text/html" width="420" height="345"
+		src="<?php echo $url; ?>" frameborder="0" allowfullscreen></iframe>
+	<?php
+}
     
