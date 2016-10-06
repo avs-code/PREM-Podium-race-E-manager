fded2104940e66fdfa48679a0fec84b7f546864b
arv187 <arv187@users.noreply.github.com>
2016-10-06 02:22:56 +0200

    Update edit_rules_mods.php

--- edit_rules_mods.php
+++ edit_rules_mods.php
@@ -1,12 +1,16 @@
-<? if(!defined("CONFIG")) exit(); ?>
-<? if(!isset($login)) { show_error("You do not have administrator rights\n"); return; } ?>
+<? if(!defined("CONFIG")) exit();
+if(!isset($login)) { show_error("You do not have administrator rights\n"); return; }
+if (isset($_POST['rules'])) {
+	$rules = mysql_real_escape_string($_POST['rules']);
+	mysql_query("UPDATE config SET `rules` = '$rules' WHERE `id` = 1 LIMIT 1");
+}
+$exe_rules = mysql_query("SELECT `rules` FROM config WHERE `id` = 1 LIMIT 1");
+list($rules) = mysql_fetch_array($exe_rules);
+mysql_free_result($exe_rules);
+$rules = htmlspecialchars($rules);
+?>
 
-<form method="post">
-<textarea id='tinyeditor' cols="50" rows="15"></textarea>
-<input type="submit" value="Save" />
+<form method="post" action="index.php?page=edit_rules_mods">
+	<textarea id="tinyeditor" name="rules" cols="50" rows="15"><?php echo $rules; ?></textarea>
+	<input type="submit" value="Save" />
 </form>
-
-<form method="post" action="show_rules.php">
-        
-</form>
-
