295ae9186442c4efc8080088751c123ee272a7f4
InGuNi <inguni@gmail.com>
2016-10-06 13:43:10 +0200

    Update sim_results.php
    Now the results from sim_results will be in the same page, so in any case the user must leave the website.

--- sim_results.php
+++ sim_results.php
@@ -1,9 +1,11 @@
 <? if (!defined("CONFIG"))
-    exit(); ?>
-<?
+    exit();
 
-$sim_results = "SELECT `sim_results`.`race_name` , `season`.`name` AS season_name, `sim_results`.`simresults_url`
-FROM sim_results, season ORDER BY `season`.`name` ASC LIMIT 0 , 30";
+if ($simresultID = intval($_GET['sres']))
+	$sim_results = "SELECT simresults_url FROM sim_results WHERE `id` = '$simresultID' LIMIT 1";
+else
+	$sim_results = "SELECT `sim_results`.`id`, `sim_results`.`race_name` , `season`.`name` AS season_name, `sim_results`.`simresults_url` FROM sim_results LEFT JOIN season ON `sim_results`.`season` = `season`.`id` ORDER BY `season`.`name` ASC LIMIT 0 , 30";
+
 $result = mysql_query($sim_results);
 if (!$result) {
     show_error("MySQL Error: " . mysql_error() . "\n");
@@ -11,22 +13,33 @@ if (!$result) {
 }
 ?>
 <h1>Simresults</h1>
-<h2>Simresults</h2>
-<div class="w3-container">
-<table class="w3-table-all">
-<tr class="w3-dark-grey">
-<td><h1>Name</h1></td>
-<td><h1><strong>Season</strong></h1></td>
-<td><h1>Simresults_URL</h1></td>
-</tr>
-<?
-while ($sitem = mysql_fetch_array($result)) {?>
-<tr class="w3-hover-green">
-<td><?= $sitem['race_name'] ?></td>
-<td><?= $sitem['season_name'] ?></td>
-<td><a href="<?= $sitem['simresults_url']; ?>" target="_blank">simresults</a></td>
-</tr><?
+<?php
+if ($simresultID) {
+	$sitem = mysql_fetch_array($result);
+	?>
+	<h2><a href="?page=sim_results">&#8606; Go back</a></h2>
+	<iframe src="<?=$sitem['simresults_url'];?>" width="100%" height="600px"></iframe>
+	<?php
+	return;
 }
 ?>
-</table>
+<div class="w3-container">
+	<table class="w3-table-all">
+		<tr class="w3-dark-grey">
+			<td><h1>Name</h1></td>
+			<td><h1><strong>Season</strong></h1></td>
+			<td><h1>Simresults_URL</h1></td>
+		</tr>
+		<?
+		while ($sitem = mysql_fetch_array($result)) {
+			?>
+			<tr class="w3-hover-green">
+				<td><?= $sitem['race_name'] ?></td>
+				<td><?= $sitem['season_name'] ?></td>
+				<td><a href="?page=sim_results&sres=<?=$sitem['id'];?>">simresults</a></td>
+			</tr>
+			<?
+		}
+		?>
+	</table>
 </div>
