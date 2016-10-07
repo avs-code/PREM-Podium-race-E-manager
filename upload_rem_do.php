9cbb3c6fccb05a3cbeffed74c34d253d0a414086
InGuNi <inguni@gmail.com>
2016-10-08 00:10:36 +0200

    Update upload_rem_do.php
    Fixed a problem with the delete file function.

--- upload_rem_do.php
+++ upload_rem_do.php
@@ -5,9 +5,13 @@ if(!isset($login)) error("You do not have administrator rights\n");
 $id = intval($_POST['id']);
 
 mysqlconnect();
-$query = "DELETE FROM uploads WHERE id='$id' LIMIT 1";
-$result = mysql_query($query);
-unlink("uploads/".$result['file']);
+$sql_fileInfo = "SELECT `file` FROM uploads WHERE id='$id' LIMIT 1";
+$exe_fileInfo = mysql_query($sql_fileInfo);
+$fileInfo = mysql_fetch_array($exe_fileInfo);
+mysql_free_result($exe_fileInfo);
+
+$result = mysql_query("DELETE FROM uploads WHERE id='$id' LIMIT 1");
+unlink("uploads/".$fileInfo['file']);
 
 if(!$result) error("MySQL Error: " . mysql_error() . "\n");
 
