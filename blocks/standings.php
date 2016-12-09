3f35a837cbd44426d4729da9b8efe1563fa7ece5
arv187 <sparkticus20@hotmail.com>
2016-12-10 00:49:16 +0100

    standings WIP

--- blocks/standings.php
+++ blocks/standings.php
@@ -1,213 +1,138 @@
+<?php
+$sql_standing_pages = "SELECT sp.id, sp.page, sp.season, s.name FROM `standing_pages` AS sp LEFT JOIN `season` AS s ON sp.season = s.id ORDER BY sp.page ASC";
+$exe_standing_pages = mysql_query($sql_standing_pages);
+if(!$exe_standing_pages) {
+	error("MySQL error: " . mysql_error() . "\n");
+}
+if(mysql_num_rows($exe_standing_pages) > 0) {
+	while(list($spID, $spPage, $spSeason, $seasonName) = mysql_fetch_array($exe_standing_pages)) {
+		$standing_pages[$spID] = array(
+			'page' => $spPage,
+			'season' => $spSeason,
+			'seasonName' => $seasonName
+		);
+	}
+	mysql_free_result($exe_standing_pages);
+}
+
+
+$sql_season_details = "SELECT `id`, `name`, `division`, `ruleset` FROM `season`";
+$exe_season_details = mysql_query($sql_season_details);
+while(list($seasonID, $seasonName, $seasonDivision, $seasonRuleset) = mysql_fetch_array($exe_season_details)) {
+	$season[$seasonID] = array(
+		'name' => $seasonName,
+		'division' => $seasonDivision,
+		'ruleset' => $seasonRuleset
+	);
+}
+mysql_free_result($exe_season_details);
+
+$sql_point_ruleset = "SELECT pr.id, pr.rp1, pr.rp2, pr.rp3, pr.rp4, pr.rp5, pr.rp6, pr.rp7, pr.rp8, pr.rp9, pr.rp10, pr.rp11, pr.rp12, pr.rp13, pr.rp14, pr.rp15 FROM `point_ruleset` AS pr";
+$exe_point_ruleset = mysql_query($sql_point_ruleset);
+while(list($prID, $prrp1, $prrp2, $prrp3, $prrp4, $prrp5, $prrp6, $prrp7, $prrp8, $prrp9, $prrp10, $prrp11, $prrp12, $prrp13, $prrp14, $prrp15) = mysql_fetch_array($exe_point_ruleset)) {
+	$point_ruleset[$prID] = array(
+		'rp1' => $prrp1,
+		'rp2' => $prrp2,
+		'rp3' => $prrp3,
+		'rp4' => $prrp4,
+		'rp5' => $prrp5,
+		'rp6' => $prrp6,
+		'rp7' => $prrp7,
+		'rp8' => $prrp8,
+		'rp9' => $prrp9,
+		'rp10' => $prrp10,
+		'rp11' => $prrp11,
+		'rp12' => $prrp12,
+		'rp13' => $prrp13,
+		'rp14' => $prrp14,
+		'rp15' => $prrp15
+	);
+}
+mysql_free_result($exe_point_ruleset);
+?>
+
 <!--Standing block-->
-
-
 
 <div>
 	
 
 	<ul class="w3-pagination">
-	  <li><a href="javascript:void(0)" class="tablink" onclick="openLink(event, '1');"><i class="w3-margin-center"></i>1</a></li>
-	  <li><a href="javascript:void(0)" class="tablink" onclick="openLink(event, '2');"><i class="w3-margin-center"></i>2</a></li>
-	  <li><a href="javascript:void(0)" class="tablink" onclick="openLink(event, '3');"><i class="w3-margin-center"></i>3</a></li>
-	  <li><a href="javascript:void(0)" class="tablink" onclick="openLink(event, '4');"><i class="w3-margin-center"></i>4</a></li>
-	  <li><a href="javascript:void(0)" class="tablink" onclick="openLink(event, '5');"><i class="w3-margin-center"></i>5</a></li>
-	  <li><a href="javascript:void(0)" class="tablink" onclick="openLink(event, '6');"><i class="w3-margin-center"></i>6</a></li>
-      <li><a href="javascript:void(0)" class="tablink" onclick="openLink(event, '7');"><i class="w3-margin-center"></i>7</a></li>
+		<?php
+		foreach ($standing_pages as $spID => $spDetails) {
+			?>
+			<li><a href="javascript:void(0)" class="tablink" onclick="openLink(event, 'standing_<?=$spID;?>');" title="<?=$spDetails['seasonName'];?>"><i class="w3-margin-center"></i><?=$spDetails['page'];?></a></li>
+			<?php
+		}
+		?>
 	</ul>
 
+	<?php
+	foreach ($standing_pages as $spID => $spDetails) {
 
-<!--
-Numbers of page (1,2,3,4,5) indicate season number in database ( for example: 1 Formula1, 2 blancpain, 3 WTCC)
--->
-
-
-<!--Page 1-->
-<!--Team standing-->
-<div id="1" class="myLink">
-<div class="w3-center w3-black w3-text-white"><h2>Team Standings</h2></div>
-
-<h3>Formula1</h3><br />
-Mercedes 	765<br />
-Red Bull Racing TAG Heuer 	468<br />
-Ferrari 	398<br />
-Force India Mercedes 	173<br />
-Williams Mercedes 	138<br />
-McLaren Honda 	76<br />
-   
-<!--Driver standing-->
-
-	<div class="w3-center w3-black w3-text-white"><h2>Driver Standings</h2></div>
-
-Nico Rosberg Mercedes 385<br />
-Lewis Hamilton Mercedes 380<br />
-Daniel Ricciardo Red Bull Racing TAG Heuer 256<br />
-Sebastian Vettel Ferrari 	212<br />
-Max Verstappen Red Bull Racing TAG Heuer 204<br />
-Kimi Räikkönen Ferrari 186<br />
-
-
-<p></p>
-
-</div>
-
-<!--Page 2-->
-<!--Team standing-->
-
-<div id="2" class="myLink">
-<div class="w3-center w3-black w3-text-white"><h2>Team Standings</h2></div>
-
-<h3>Blancpain GT Series</h3><br />
-
-HTP Motorsport 167<br />
-Belgian Audi Club Team WRT 160<br />
-Belgian Audi Club Team WRT 133<br />
-Garage 59 130<br />
-Bentley Team M-Sport 126<br />
-ROWE Racing 96<br />
-GRT Grasser Racing Team 86<br />
-Akka ASP 80<br />
-Sainteloc Racing 56<br />
-ISR 45<br />
-	
-<!--Driver standing-->
-
-	<div class="w3-center w3-black w3-text-white"><h2>Driver Standings</h2></div>
-
-Maximilian Buhk 134<br />
-Dominik Baumann 134<br />
-Rob Bell 124<br />
-Laurens Vanthoor 112<br />
-Maxime Soulet 106<br />
-Andy Soucek 106<br />
-Frederic Vervisch 103<br />
-Enzo Ide 102<br />
-Christopher Mies 97<br />
-Philipp Eng 79<br />
+		?>
+		<div id="standing_<?=$spID;?>" class="myLink">
+			
+			
+			<!--Team standing-->
+			<div class="w3-center w3-black w3-text-white"><h2><?=$spDetails['seasonName'];?></div></h2><div class="w3-center w3-indigo w3-text-white"><h3>Team Standings</h3></div>
+			<?php
+			$sql_teams = "SELECT t.id, t.name FROM `team` AS t LEFT JOIN `season_team` AS st ON st.team = t.id WHERE st.season = ".intval($spDetails['season']);
+			$exe_teams = mysql_query($sql_teams);
+			while(list($teamID, $teamName) = mysql_fetch_array($exe_teams)) {
+				echo $teamName."<br/>";
+			}
+			mysql_free_result($exe_teams);
+			?>
+			<!--Mercedes 	765<br />
+			Red Bull Racing TAG Heuer 	468<br />
+			Ferrari 	398<br />
+			Force India Mercedes 	173<br />
+			Williams Mercedes 	138<br />
+			McLaren Honda 	76<br />-->
+			   
+			<!--Driver standing-->
+			<div class="w3-center w3-indigo w3-text-white"><h3>Driver Standings</h3></div>
+			<?php
+			$sql_drivers = "SELECT d.id did, d.name dname FROM season_team st JOIN team t ON (st.team = t.id) JOIN team_driver td ON (td.team = t.id) JOIN driver d ON (d.id = td.driver) WHERE st.season = '".intval($spDetails['season'])."' ORDER BY t.name ASC, d.name ASC";
+			$exe_drivers = mysql_query($sql_drivers);
+			while(list($driverID, $driverName) = mysql_fetch_array($exe_drivers)) {
+				echo $driverName."<br/>";
+			}
+			mysql_free_result($exe_drivers);
+			?>
+			<!--
+			Nico Rosberg Mercedes 385<br />
+			Lewis Hamilton Mercedes 380<br />
+			Daniel Ricciardo Red Bull Racing TAG Heuer 256<br />
+			Sebastian Vettel Ferrari 	212<br />
+			Max Verstappen Red Bull Racing TAG Heuer 204<br />
+			Kimi Räikkönen Ferrari 186<br />-->
 
 
+			<p></p>
+		</div>
+		<?php
+	}
+	?>
 
-<p></p>
-</div>
-
-<!--Page 3-->
-<!--Team standing-->
-
-<div id="3" class="myLink">
-<div class="w3-center w3-black w3-text-white"><h2>Team Standings</h2></div>
-	  Show season.name nº3<br />
-	  Show Team - Points<br />
-	
-<!--Driver standing-->
-
-	<div class="w3-center w3-black w3-text-white"><h2>Driver Standings</h2></div>
-
-show driver name - points
-
-
-<p></p>
-
-</div>
-
-<!--Page 4-->
-<!--Team standing-->
-
-
-<div id="4" class="myLink">
-<div class="w3-center w3-black w3-text-white"><h2>Team Standings</h2></div>
-	  Show season.name nº4<br />
-	  Show Team - Points<br />
-	
-<!--Driver standing-->
-
-	<div class="w3-center w3-black w3-text-white"><h2>Driver Standings</h2></div>
-
-show driver name - points
-
-
-<p></p>
-
-</div>
-
-<!--Page 5-->
-<!--Team standing-->
-
-
-<div id="5" class="myLink">
-<div class="w3-center w3-black w3-text-white"><h2>Team Standings</h2></div>
-	  Show season.name nº5<br />
-	  Show Team - Points<br />
-	
-<!--Driver standing-->
-
-	<div class="w3-center w3-black w3-text-white"><h2>Driver Standings</h2></div>
-
-show driver name - points
-
-
-<p></p>
-
-</div>
-
-<!--Page 6-->
-<!--Team standing-->
-
-
-<div id="6" class="myLink">
-<div class="w3-center w3-black w3-text-white"><h2>Team Standings</h2></div>
-	  Show season.name nº6<br />
-	  Show Team - Points<br />
-	
-<!--Driver standing-->
-
-	<div class="w3-center w3-black w3-text-white"><h2>Driver Standings</h2></div>
-
-show driver name - points
-
-
-<p></p>
-
-</div>
-
-<!--Page 7-->
-<!--Team standing-->
-
-
-<div id="7" class="myLink">
-<div class="w3-center w3-black w3-text-white"><h2>Team Standings</h2></div>
-	  Show season.name nº7<br />
-	  Show Team - Points<br />
-	
-<!--Driver standing-->
-
-	<div class="w3-center w3-black w3-text-white"><h2>Driver Standings</h2></div>
-
-show driver name - points
-
-
-<p></p>
-
-</div>
-
-
-
-<script>
-// Team and driver Standings
-function openLink(evt, linkName) {
-  var i, x, tablinks;
-  x = document.getElementsByClassName("myLink");
-  for (i = 0; i < x.length; i++) {
-	  x[i].style.display = "none";
-  }
-  tablinks = document.getElementsByClassName("tablink");
-  for (i = 0; i < x.length; i++) {
-	  tablinks[i].className = tablinks[i].className.replace(" w3-red", "");
-  }
-  document.getElementById(linkName).style.display = "block";
-  evt.currentTarget.className += " w3-red";
-}
-// Click on the first tablink on load
-document.getElementsByClassName("tablink")[0].click();
-</script>
+	<script>
+		// Team and driver Standings
+		function openLink(evt, linkName) {
+			var i, x, tablinks;
+			x = document.getElementsByClassName("myLink");
+			for (i = 0; i < x.length; i++) {
+				x[i].style.display = "none";
+			}
+			tablinks = document.getElementsByClassName("tablink");
+			for (i = 0; i < x.length; i++) {
+				tablinks[i].className = tablinks[i].className.replace(" w3-red", "");
+			}
+			document.getElementById(linkName).style.display = "block";
+			evt.currentTarget.className += " w3-red";
+		}
+		// Click on the first tablink on load
+		document.getElementsByClassName("tablink")[0].click();
+	</script>
 
 
 </div>
