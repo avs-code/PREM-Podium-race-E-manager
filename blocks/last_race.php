82d837829a333473288d6d5231db4025a599005a
arv187 <sparkticus20@hotmail.com>
2016-12-10 02:12:49 +0100

    updated WIP

--- blocks/last_race.php
+++ blocks/last_race.php
@@ -1,6 +1,53 @@
 <!--Last race block-->
+<?php
+require_once("results_functions.php");
+
+
+
+$query = "SELECT r.*, s.name sname, d.name dname FROM race_driver rd, race r LEFT JOIN season s ON (s.id = r.season) JOIN division d ON (d.id = r.division) WHERE ((r.id =rd.race) AND (r.progress = 2) AND (rd.status =0)) ORDER BY r.date DESC, rd.position ASC LIMIT 1";
+$result = mysql_query($query);
+if(!$result) {
+	show_error("MySQL Error: " . mysql_error() . "\n");
+	return;
+}
+if(mysql_num_rows($result) == 0) {
+	show_error("Race does not exist\n");
+	return;
+}
+
+$item = mysql_fetch_array($result);
+
+$last = ($item['id']);
+
+$dquery = "SELECT rd.*, d.name dname, t.name tname FROM race_driver rd JOIN team_driver td ON (td.id = rd.team_driver) JOIN team t ON (t.id = td.team) JOIN driver d ON (d.id = td.driver) JOIN race r ON (rd.race = r.id) WHERE rd.race='$last' AND (rd.status = 0) ORDER BY rd.position ASC LIMIT 3";
+$dresult = mysql_query($dquery);
+if(!$dresult) {
+	show_error("MySQL Error: " . mysql_error() . "\n");
+	return;
+}
+
+$ditem = mysql_fetch_array($dresult)
+
+?>
 <div>
-	
+<table>
+
+    <td><?=$item['sname']?></td>
+    <tr></tr>
+	<td><strong>Track:&nbsp;</strong><?=$item['name']?></td>
+    <tr></tr>
+    <td><strong>Pos</strong></td><td><strong>Driver</strong></td><td><strong>Team</strong></td>
+    <tr></tr>
+    <td><?=++$position?></td>
+    <td><?=$ditem['dname']?></td>
+	<td><?=$ditem['tname']?></td>
+
+</table>
+</div>
+
+
+<div>
+<strong>Final example:</strong><br />
 Show race.season<br />
 Show race.name<br />
 1º driver name - team<br />
