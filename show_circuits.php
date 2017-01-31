44d2ebf50de4cdde7699b682b06a0f95669859c2
arv187 <sparkticus20@hotmail.com>
2017-01-31 21:18:22 +0100

    Changed season column for division

--- show_circuits.php
+++ show_circuits.php
@@ -1,37 +1,37 @@
-<? if (!defined("CONFIG"))
-    exit(); ?>
-<?
-$circuits = "SELECT race.track, race.name, race.division, race.date, race.imagelink, race.maxplayers, race.season, season.name AS season_name FROM race INNER JOIN season on season.id=race.season ORDER BY race.season ASC, race.date ASC LIMIT 0, 30";
+<? if (!defined("CONFIG")) exit();
+
+
+$circuits = "SELECT race.track, race.name, race.division, race.date, race.imagelink, race.maxplayers, race.season, division.name AS division_name
+    	FROM race LEFT JOIN division on division.id=race.division WHERE race.date>=CURDATE() ORDER BY race.division ASC, race.date ASC LIMIT 0, 30";
 $result = mysql_query($circuits);
 if (!$result) {
     show_error("MySQL Error: " . mysql_error() . "\n");
     return;
 }
 ?>
-<h1>Circuits</h1>
-<h2>Circuits</h2>
+
+<h1>Circuits Calendar</h1>
+<h2>Circuits ordered by division</h2>
+
 <div class="w3-container">
 <table class="w3-table-all">
-<tr class="w3-dark-grey">
-	<td>Name</td>
-	<td>Track</td>
-	<td>Season</td>
-    <td>Date</td>
-    <td>Image</td>
-</tr>
-<?
-#$style = "odd";
-while ($sitem = mysql_fetch_array($result)) { ?>
-<!--<tr class="<?= $style ?>">-->
-<tr class="w3-hover-blue">
-<td><?= $sitem['name'] ?></td>
-<td><?= $sitem['track'] ?></td>
-<td><?= $sitem['season_name'] ?></td>
-<td><?= $sitem['date'] ?></td>
-<td><a href="<?= $sitem['imagelink'] ?>" target="_blank"><img src="<?= $sitem['imagelink']; ?>" width="250" height="165"/></a></td>
-</tr>
-<?
-#$style = $style == "odd" ? "even" : "odd";
-}
-?>
+    <tr class="w3-dark-grey">
+    	<td>Name</td>
+    	<td>Track</td>
+    	<td>Division</td>
+        <td>Date</td>
+        <td>Image</td>
+    </tr>
+    
+<? while ($sitem = mysql_fetch_array($result)) { ?>
+
+    <tr class="w3-hover-blue">
+        <td><?= $sitem['name'] ?></td>
+        <td><?= $sitem['track'] ?></td>
+        <td><?= $sitem['division_name'] ?></td>
+        <td><?= $sitem['date'] ?></td>
+        <td><a href="<?= $sitem['imagelink'] ?>" target="_blank"><img src="<?= $sitem['imagelink']; ?>" width="250" height="165"/></a></td>
+    </tr>
+    
+<? } ?>
 </table>
