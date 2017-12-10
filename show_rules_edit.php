8cc7d717b87adafb6970f3066dc00036a5edc181
busterwolfeman <wolfgang@netracingeurope.org>
2017-12-10 12:26:49 +0100

    adds the possibility to add and edit several sets of regulations

--- show_rules_edit.php
+++ show_rules_edit.php
@@ -17,7 +17,7 @@ if(!$result) {
 <h1>Regulations</h1>
 <?php
 $name = $_POST["name"];
-if(mysqli_num_rows($link,$result) == 0) {
+if(mysqli_num_rows($result) == 0) {
 	show_msg("No Regulations found\n");
 	return;
 }
